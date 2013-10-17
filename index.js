fb89534771418f4f516830b9fcc4e48e3a488b3a
Parsha Pourkhomami <parshap+git@gmail.com>
2013-10-17 02:04:54 -0700

    Working implementation

---
+++ index.js
@@ -0,0 +1,50 @@
+// Takes plain text and replaces links with HTML anchor elements. Based on
+// https://github.com/thejh/node-linkify and
+// http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+
+"use strict";
+
+var r = /\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi;
+
+var escape = require("html-escape");
+
+module.exports = function(text, options) {
+	if ( ! options) options = {};
+
+	var retval = "", cur = 0, match;
+
+	while (match = r.exec(text)) {
+		retval += escape(text.slice(cur, match.index));
+		retval += anchor(match[0], options.attributes);
+		cur = r.lastIndex;
+	}
+
+	retval += escape(text.slice(cur));
+
+	return retval;
+};
+
+// Return an anchor element for the url
+function anchor(url, attrs) {
+	var text = escape(url),
+		href = url;
+
+	// Ensure protocol at beginning of url
+	if (!/^[a-zA-Z]{1,6}:/.test(href)) {
+		href = 'http://' + href;
+	}
+
+	var attrsString = [attributes({ href: href }), attributes(attrs)]
+		.filter(Boolean)
+		.join(" ");
+
+	return "<a "+ attrsString + ">" + text + "</a>";
+}
+
+function attributes(attrs) {
+	if ( ! attrs) return "";
+	return Object.keys(attrs).map(function(name) {
+		var value = attrs[name];
+		return escape(name) + "=\"" + escape(value) + "\"";
+	}).join(" ");
+}
